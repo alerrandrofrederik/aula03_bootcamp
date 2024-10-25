746a398fa2b97f027a0dfe1506c02a3238a4a494
Alerrandro <alerrandrofrederik@gmail.com>
2024-10-25 14:20:51 -0300

    desafio aula 03

---
+++ desafio.py
@@ -0,0 +1,11 @@
+
+
+# nome = input("digite seu nome: ")
+# salario = float(input("digite seu salario: "))
+# bonus_percentual = float(input("digite o percentual de bonus: "))
+# kpi = 1000 + salario * bonus_percentual
+
+# print(f"Olá {nome}, seu bônus foi de {kpi}")
+
+#Integre na solução anterior um fluxo de While que repita o fluxo até que o usuário insira as informações corretas
+
